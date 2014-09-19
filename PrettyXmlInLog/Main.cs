c7b7c77b998b3fdf5acb14bd4f73028d0d97a382
unknown <pavlo.khlostkin@KBP1-LHP-F55547.synapse.com>
2014-09-19 20:50:24 +0300

    Demo code is removed

--- PrettyXmlInLog/Main.cs
+++ PrettyXmlInLog/Main.cs
@@ -9,94 +9,84 @@ using NppPluginNET;
 
 namespace PrettyXmlInLog
 {
-    class Main
-    {
-        #region " Fields "
-        internal const string PluginName = "PrettyXmlInLog";
-        static string iniFilePath = null;
-        static bool someSetting = false;
-        static frmMyDlg frmMyDlg = null;
-        static int idMyDlg = -1;
-        static Bitmap tbBmp = Properties.Resources.star;
-        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
-        static Icon tbIcon = null;
-        #endregion
+	class Main
+	{
+		#region Fields
 
-        #region " StartUp/CleanUp "
-        internal static void CommandMenuInit()
-        {
-            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
-            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
-            iniFilePath = sbIniFilePath.ToString();
-            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
-            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
-            someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+		internal const string PluginName = "Pretty Xml In Log";
 
-			PluginBase.SetCommand(0, "MyMenuCommand", myMenuFunction, new ShortcutKey(true, true, true, Keys.L));
-            PluginBase.SetCommand(1, "MyDockableDialog", myDockableDialog); idMyDlg = 1;
-        }
-        internal static void SetToolBarIcon()
-        {
-            toolbarIcons tbIcons = new toolbarIcons();
-            tbIcons.hToolbarBmp = tbBmp.GetHbitmap();
-            IntPtr pTbIcons = Marshal.AllocHGlobal(Marshal.SizeOf(tbIcons));
-            Marshal.StructureToPtr(tbIcons, pTbIcons, false);
-            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_ADDTOOLBARICON, PluginBase._funcItems.Items[idMyDlg]._cmdID, pTbIcons);
-            Marshal.FreeHGlobal(pTbIcons);
-        }
-        internal static void PluginCleanUp()
-        {
-            Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
-        }
-        #endregion
+		#endregion
 
-        #region " Menu functions "
-        internal static void myMenuFunction()
-        {
-	        IntPtr hCurrentEditView = PluginBase.GetCurrentScintilla();
-	        var length = (int)Win32.SendMessage(hCurrentEditView, SciMsg.SCI_GETCURLINE, 0, 0);
-	        
-			var lineStringBuilder = new StringBuilder(length);
-			Win32.SendMessage(hCurrentEditView, SciMsg.SCI_GETCURLINE, length, lineStringBuilder);
-	        MessageBox.Show(lineStringBuilder.ToString());
-			
-        }
-        internal static void myDockableDialog()
-        {
-            if (frmMyDlg == null)
-            {
-                frmMyDlg = new frmMyDlg();
+		#region StartUp/CleanUp
 
-                using (Bitmap newBmp = new Bitmap(16, 16))
-                {
-                    Graphics g = Graphics.FromImage(newBmp);
-                    ColorMap[] colorMap = new ColorMap[1];
-                    colorMap[0] = new ColorMap();
-                    colorMap[0].OldColor = Color.Fuchsia;
-                    colorMap[0].NewColor = Color.FromKnownColor(KnownColor.ButtonFace);
-                    ImageAttributes attr = new ImageAttributes();
-                    attr.SetRemapTable(colorMap);
-                    g.DrawImage(tbBmp_tbTab, new Rectangle(0, 0, 16, 16), 0, 0, 16, 16, GraphicsUnit.Pixel, attr);
-                    tbIcon = Icon.FromHandle(newBmp.GetHicon());
-                }
+		internal static void CommandMenuInit()
+		{
+			PluginBase.SetCommand(0, "Format Line", FormatLine, new ShortcutKey(true, true, true, Keys.L));
+			PluginBase.SetCommand(1, "Format Selection", FormatSelection, new ShortcutKey(true, true, true, Keys.S));
+		}
 
-                NppTbData _nppTbData = new NppTbData();
-                _nppTbData.hClient = frmMyDlg.Handle;
-                _nppTbData.pszName = "My dockable dialog";
-                _nppTbData.dlgID = idMyDlg;
-                _nppTbData.uMask = NppTbMsg.DWS_DF_CONT_RIGHT | NppTbMsg.DWS_ICONTAB | NppTbMsg.DWS_ICONBAR;
-                _nppTbData.hIconTab = (uint)tbIcon.Handle;
-                _nppTbData.pszModuleName = PluginName;
-                IntPtr _ptrNppTbData = Marshal.AllocHGlobal(Marshal.SizeOf(_nppTbData));
-                Marshal.StructureToPtr(_nppTbData, _ptrNppTbData, false);
+		internal static void SetToolBarIcon()
+		{
+		}
 
-                Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_DMMREGASDCKDLG, 0, _ptrNppTbData);
-            }
-            else
-            {
-                Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_DMMSHOW, 0, frmMyDlg.Handle);
-            }
-        }
-        #endregion
-    }
+		internal static void PluginCleanUp()
+		{
+		}
+
+		#endregion
+
+		#region Menu functions
+
+		internal static void FormatLine()
+		{
+			try
+			{
+				var line = GetCurrentLine();
+				MessageBox.Show(line);
+			}
+			catch (Exception ignore)
+			{ }
+		}
+
+		internal static void FormatSelection()
+		{
+			try
+			{
+				var selection = GetSelection();
+				MessageBox.Show(selection);
+			}
+			catch (Exception ignore)
+			{ }
+		}
+
+		private static String GetCurrentLine()
+		{
+			return GetText(SciMsg.SCI_GETCURLINE);
+		}
+
+		private static string GetSelection()
+		{
+			return GetText(SciMsg.SCI_GETSELTEXT);
+		}
+
+		private static string GetText(SciMsg sciMsg)
+		{
+			var hCurrentEditView = PluginBase.GetCurrentScintilla();
+			var length = (int)Win32.SendMessage(hCurrentEditView, sciMsg, 0, 0);
+
+			if (length > 0)
+			{
+				var lineStringBuilder = new StringBuilder(length);
+				Win32.SendMessage(hCurrentEditView, sciMsg, length, lineStringBuilder);
+
+				return lineStringBuilder.ToString();
+			}
+			else
+			{
+				return String.Empty;
+			}
+		}
+
+		#endregion
+	}
 }
